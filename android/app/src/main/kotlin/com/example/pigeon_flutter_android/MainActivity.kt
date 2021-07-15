e990aaedce580563c119da00b18443dfec492e8f
Israel Hudson <israelhudson@gmail.com>
2021-07-15 19:49:56 -0300

    gerando codigo nativo com dart

--- android/app/src/main/kotlin/com/example/pigeon_flutter_android/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pigeon_flutter_android/MainActivity.kt
@@ -3,4 +3,13 @@ package com.example.pigeon_flutter_android
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+   //val pigeon = Pigeon
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        super.configureFlutterEngine(flutterEngine)
+//        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+//            call, result ->
+//            // Note: this method is invoked on the main thread.
+//            // TODO
+//        }
+//    }
 }
